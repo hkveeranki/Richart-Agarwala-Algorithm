bccc35944c07293974c4245c3815885db8c6b97a
Hemanth Kumar Veeranki <hems.india1997@gmail.com>
2016-09-14 16:26:37 +0530

    Made Some changes

--- src/Node.java
+++ src/Node.java
@@ -5,18 +5,19 @@
 import java.util.PriorityQueue;
 
 public class Node implements Nodedef {
-    PriorityQueue<Message> queue;
-    boolean is_in_critical;
+    private PriorityQueue<Request> queue;
+    private boolean is_in_critical;
+    static Nodedef[] remote_objects;
 
     public Node() {
         super();
-        this.queue = new PriorityQueue<>();
+        this.queue = new PriorityQueue<>(new RequestComparator());
         this.is_in_critical = false;
     }
 
     public void LockRequest(byte[] info) {
     /* Other Objects send lock  request to this node using this Remote Method */
-        Message lock_request = new Message(info);
+        Request lock_request = new Request(info);
         if (is_in_critical) {
             queue.add(lock_request);
         } else {
@@ -36,18 +37,30 @@ public class Node implements Nodedef {
 
 
     public static void main(String[] args) {
+        /* Initialisations and stuff */
 
+        int N = Integer.valueOf(args[1]);
+        int my_id = Integer.valueOf(args[0]);
+        remote_objects = new Nodedef[N];
+        /* Getting all those Mappings */
+
+        /* Running the given loop */
+        for (int i = 0; i < 100; i++) {
+            lock();
+            /* Critical Section */
+            unlock();
+        }
     }
 
-    public static void send_ack() {
+    private static void send_ack() {
         /* Send Ack to particular Node */
     }
 
-    public static void lock() {
+    private static void lock() {
         /* Perform Mechanism for locking */
     }
 
-    public static void unlock() {
+    private static void unlock() {
         /* Perform Mechanism for unlocking */
     }
 }
